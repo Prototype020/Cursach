5b2a075fcfd1e6d15712ae3713e167d8fedf56e3
Prototype020 <silver2371@mail.ru>
2025-02-19 16:50:46 +0300

    add classes Pointer and Line

---
+++ ElementsMovement.js
@@ -0,0 +1,124 @@
+let svgMain = document.querySelector('#svgMain')
+let ns = 'http://www.w3.org/2000/svg'
+
+let defaultSetings = {
+    stroke: 'white',
+    strokeWidth: '2px',
+    fill: 'black'
+}
+
+
+class Line {
+
+    constructor(determ) {
+        this.pointers = []
+        this.createLine()
+        this._setDefaultSettings()
+        
+       
+        this.updateLine()
+        
+    }
+
+    _setDefaultSettings() {
+        console.log(this.pointers)
+        this.createPointer([(field.clientWidth/2 + field.scrollLeft),(field.clientHeight/2 + field.scrollTop)])
+        this.createPointer([(field.clientWidth/2 + field.scrollLeft + 50),(field.clientHeight/2 + field.scrollTop)])
+         
+        this.stroke = defaultSetings.stroke
+        this.strokeWidth = defaultSetings.strokeWidth
+
+
+    }
+
+    createPointer(cords) {
+        let pointer = new Pointer(cords)
+        this.pointers.push(pointer)
+
+        let line = this.updateLine.bind(this)
+        function Movepointer(event) {
+            let X = event.pageX - (event.clientX - event.target.getBoundingClientRect().left)-svgMain.getBoundingClientRect().left
+            let Y = event.pageY - (event.clientY - event.target.getBoundingClientRect().top)- svgMain.getBoundingClientRect().top
+
+            pointer.changeCords(X,Y)
+            line()
+        }
+
+        pointer.point.addEventListener('mousedown', function() {
+            pointer.point.addEventListener('mousemove' , Movepointer)
+        })
+        pointer.point.addEventListener('mouseup', function() {
+            pointer.point.removeEventListener('mousemove' , Movepointer)
+        })
+    }
+        
+        
+        
+        
+
+    updateLine() {
+        this.line.setAttributeNS(null,'d',`M${this.getcords()}`)
+    }
+    
+
+    addIntermediatePoints() {
+
+    }
+
+    getcords() {
+        return this.pointers.map((item) => item.cords).join(' ').replaceAll(',', ' ')
+    }
+
+    createLine() {
+        let line = document.createElementNS(ns,'path')
+        line.setAttributeNS(null,'stroke',defaultSetings.stroke)
+        line.setAttributeNS(null,'stroke-width', defaultSetings.strokeWidth)
+        svgMain.insertAdjacentElement('afterbegin', line)
+        this.line = line
+        
+    }
+
+    
+    
+}
+
+
+
+
+
+
+
+
+class Pointer{
+
+    constructor(cords, obj) {
+        this.cords = cords
+        this.point = this.createPointer(cords)
+        
+    }
+
+    createPointer() {
+        let pointer = document.createElementNS(ns,'circle')
+        pointer.setAttributeNS(null,'cx', this.cords[0])
+        pointer.setAttributeNS(null,'cy', this.cords[1])
+        pointer.setAttributeNS(null,'r', 5)
+        pointer.setAttributeNS(null,'fill', 'white')
+        pointer.setAttribute(null,'draggable', 'true')
+        pointer.cords = this.cords
+        
+        
+        svgMain.insertAdjacentElement('afterbegin',pointer)
+        
+        return pointer
+
+    }
+
+    changeCords(x,y) {
+        this.cords = [x,y]
+        this.point.setAttributeNS(null,'cx', x)
+        this.point.setAttributeNS(null,'cy', y)
+    }
+}
+
+let line = new Line()
+let point = new Pointer([40,40])
